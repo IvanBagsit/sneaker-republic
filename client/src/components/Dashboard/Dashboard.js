b8afcda595a7a1c08cda6bea4c969ba202951fb1
ivanbagsit <ivan.bagsit@novare.com.hk>
2023-10-17 17:38:33 +0800

    fix: flickering of sidebar during reload of page

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,20 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Dashboard.module.css";
 import GetStarted from "./GetStarted";
 import Sidebar from "../Sidebar/Sidebar";
 
 const Dashboard = () => {
     const [isGetStartedClosed, setIsGetStartedClosed] = useState(false);
+    const [isPageLoaded, setIsPageLoaded] = useState(false);
 
     const handleGetStarted = (data) => {
         setIsGetStartedClosed(data);
     };
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsPageLoaded(true);
+        }, 600);
+    }, []);
+
     return (
         <div className={styles.background}>
             {!isGetStartedClosed && (
                 <GetStarted isGetStartedClosed={handleGetStarted} />
             )}
-            <Sidebar />
+            {isPageLoaded && <Sidebar />}
         </div>
     );
 };
