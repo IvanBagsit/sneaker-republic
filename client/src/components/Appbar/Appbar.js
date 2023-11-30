8a012f8d2bc2f5b99449f98110b0068dfeec8ae9
ivanbagsit <ivan.bagsit@novare.com.hk>
2023-11-30 15:16:38 +0800

    code enhancements

--- client/src/components/Appbar/Appbar.js
+++ client/src/components/Appbar/Appbar.js
@@ -1,16 +1,8 @@
 import styles from "./Appbar.module.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import Scrollbar from "../Common/Scrollbar";
-import { Link } from "react-router-dom";
-import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
-    Stack,
-    Typography,
-} from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useState } from "react";
+import MenuOptions from "../Common/menu/MenuOptions.js";
 
 const Appbar = () => {
     const options = [
@@ -118,111 +110,9 @@ const Appbar = () => {
                 </div>
             </div>
             {isMenuOpen && (
-                <Scrollbar maxHeight={"30vh"}>
+                <Scrollbar maxHeight={"40vh"}>
                     {options.map((items) => {
-                        return (
-                            <Accordion
-                                key={items.name}
-                                expanded={!items.isDropDown ? false : undefined}
-                            >
-                                {items.haslink && (
-                                    <Link
-                                        to={items.link}
-                                        className={styles.link}
-                                    >
-                                        <AccordionSummary
-                                            expandIcon={
-                                                items.isDropDown ? (
-                                                    <ExpandMoreIcon />
-                                                ) : null
-                                            }
-                                        >
-                                            <Typography
-                                                style={{ fontFamily: "unset" }}
-                                            >
-                                                {items.name}
-                                            </Typography>
-                                        </AccordionSummary>
-                                    </Link>
-                                )}
-
-                                {!items.haslink && (
-                                    <AccordionSummary
-                                        expandIcon={
-                                            items.isDropDown ? (
-                                                <ExpandMoreIcon />
-                                            ) : null
-                                        }
-                                    >
-                                        <Typography
-                                            style={{ fontFamily: "unset" }}
-                                        >
-                                            {items.name}
-                                        </Typography>
-                                    </AccordionSummary>
-                                )}
-
-                                {items.subOptions.map((items) => {
-                                    return (
-                                        <Accordion
-                                            key={items.brand}
-                                            expanded={
-                                                !items.isDropDown
-                                                    ? false
-                                                    : undefined
-                                            }
-                                        >
-                                            <AccordionSummary
-                                                expandIcon={
-                                                    items.isDropDown ? (
-                                                        <ExpandMoreIcon />
-                                                    ) : null
-                                                }
-                                                style={{
-                                                    backgroundColor: "#E8E8E8",
-                                                }}
-                                            >
-                                                <span
-                                                    style={{ marginLeft: "5%" }}
-                                                >
-                                                    {items.brand}
-                                                </span>
-                                            </AccordionSummary>
-                                            {items.shoes.map((items) => {
-                                                const templink = items.replace(
-                                                    " ",
-                                                    ""
-                                                );
-                                                const link =
-                                                    templink.toLowerCase();
-                                                return (
-                                                    <Link
-                                                        to={`/view?shoes=${link}`}
-                                                        key={items}
-                                                        className={styles.link}
-                                                    >
-                                                        <AccordionDetails
-                                                            className={
-                                                                styles.shoesOption
-                                                            }
-                                                        >
-                                                            <span
-                                                                style={{
-                                                                    marginLeft:
-                                                                        "10%",
-                                                                }}
-                                                            >
-                                                                {items}
-                                                            </span>
-                                                        </AccordionDetails>
-                                                    </Link>
-                                                );
-                                            })}
-                                        </Accordion>
-                                    );
-                                })}
-                            </Accordion>
-                        );
+                        return <MenuOptions items={items} />;
                     })}
                 </Scrollbar>
             )}
---
+++ client/src/components/Common/menu/MenuOptions.js
@@ -0,0 +1,89 @@
+import styles from "./MenuOptions.module.css";
+import { Link } from "react-router-dom";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const MenuOptions = ({ items }) => {
+    return (
+        <Accordion
+            key={items.name}
+            expanded={!items.isDropDown ? false : undefined}
+        >
+            {items.haslink && (
+                <Link to={items.link} className={styles.link}>
+                    <AccordionSummary
+                        expandIcon={
+                            items.isDropDown ? <ExpandMoreIcon /> : null
+                        }
+                    >
+                        <Typography style={{ fontFamily: "unset" }}>
+                            {items.name}
+                        </Typography>
+                    </AccordionSummary>
+                </Link>
+            )}
+
+            {!items.haslink && (
+                <AccordionSummary
+                    expandIcon={items.isDropDown ? <ExpandMoreIcon /> : null}
+                >
+                    <Typography style={{ fontFamily: "unset" }}>
+                        {items.name}
+                    </Typography>
+                </AccordionSummary>
+            )}
+
+            {items.subOptions.map((items) => {
+                return (
+                    <Accordion
+                        key={items.brand}
+                        expanded={!items.isDropDown ? false : undefined}
+                    >
+                        <AccordionSummary
+                            expandIcon={
+                                items.isDropDown ? <ExpandMoreIcon /> : null
+                            }
+                            style={{
+                                backgroundColor: "#E8E8E8",
+                            }}
+                        >
+                            <span style={{ marginLeft: "5%" }}>
+                                {items.brand}
+                            </span>
+                        </AccordionSummary>
+                        {items.shoes.map((items) => {
+                            const templink = items.replace(" ", "");
+                            const link = templink.toLowerCase();
+                            return (
+                                <Link
+                                    to={`/view?shoes=${link}`}
+                                    key={items}
+                                    className={styles.link}
+                                >
+                                    <AccordionDetails
+                                        className={styles.shoesOption}
+                                    >
+                                        <span
+                                            style={{
+                                                marginLeft: "10%",
+                                            }}
+                                        >
+                                            {items}
+                                        </span>
+                                    </AccordionDetails>
+                                </Link>
+                            );
+                        })}
+                    </Accordion>
+                );
+            })}
+        </Accordion>
+    );
+};
+
+export default MenuOptions;
