24cab4af71f3b9b182b5790e02e924cccb519bdd
ivanbagsit <ivan.bagsit@novare.com.hk>
2023-12-04 17:54:47 +0800

    made .JPG to .jpg extension

--- server/common/shoes.js
+++ server/common/shoes.js
@@ -1,51 +1,51 @@
 const baseUrl = process.env.BASE_URL;
 
-const AF1Unisex1 = `${baseUrl}/Nike/AF1/Unisex 1.JPG`;
-const AF1Women5 = `${baseUrl}/Nike/AF1/Women size 5.JPG`;
-const AF1Women6 = `${baseUrl}/Nike/AF1/Women size.JPG`;
-const AF1Women2 = `${baseUrl}/Nike/AF1/Women size 2.JPG`;
+const AF1Unisex1 = `${baseUrl}/Nike/AF1/Unisex 1.jpg`;
+const AF1Women5 = `${baseUrl}/Nike/AF1/Women size 5.jpg`;
+const AF1Women6 = `${baseUrl}/Nike/AF1/Women size.jpg`;
+const AF1Women2 = `${baseUrl}/Nike/AF1/Women size 2.jpg`;
 
-const J1Unisex2 = `${baseUrl}/Nike/J1/Unisex 2.JPG`;
-const J1Men1 = `${baseUrl}/Nike/J1/Men Size only 1.JPG`;
-const J1Unisex3 = `${baseUrl}/Nike/J1/Unisex 3.JPG`;
-const J1Unisex5 = `${baseUrl}/Nike/J1/Unisex.JPG`;
+const J1Unisex2 = `${baseUrl}/Nike/J1/Unisex 2.jpg`;
+const J1Men1 = `${baseUrl}/Nike/J1/Men Size only 1.jpg`;
+const J1Unisex3 = `${baseUrl}/Nike/J1/Unisex 3.jpg`;
+const J1Unisex5 = `${baseUrl}/Nike/J1/Unisex.jpg`;
 
-const J3Men4 = `${baseUrl}/Nike/J3/Men size.JPG`;
-const J3Men1 = `${baseUrl}/Nike/J3/Men size 1.JPG`;
-const J3Men2 = `${baseUrl}/Nike/J3/Men size 2.JPG`;
-const J3Men3 = `${baseUrl}/Nike/J3/Men size 3.JPG`;
+const J3Men4 = `${baseUrl}/Nike/J3/Men size.jpg`;
+const J3Men1 = `${baseUrl}/Nike/J3/Men size 1.jpg`;
+const J3Men2 = `${baseUrl}/Nike/J3/Men size 2.jpg`;
+const J3Men3 = `${baseUrl}/Nike/J3/Men size 3.jpg`;
 
-const Airmax97Women = `${baseUrl}/Nike/Airmax 97/Women size.JPG`;
-const Airmax97Men = `${baseUrl}/Nike/Airmax 97/Men size.JPG`;
-const Airmax97Unisex1 = `${baseUrl}/Nike/Airmax 97/Unisex 1.JPG`;
-const Airmax97Unisex2 = `${baseUrl}/Nike/Airmax 97/Unisex.JPG`;
+const Airmax97Women = `${baseUrl}/Nike/Airmax 97/Women size.jpg`;
+const Airmax97Men = `${baseUrl}/Nike/Airmax 97/Men size.jpg`;
+const Airmax97Unisex1 = `${baseUrl}/Nike/Airmax 97/Unisex 1.jpg`;
+const Airmax97Unisex2 = `${baseUrl}/Nike/Airmax 97/Unisex.jpg`;
 
-const FragmentUnisex1 = `${baseUrl}/Nike/Fragment/Unisex 1.JPG`;
-const FragmentUnisex2 = `${baseUrl}/Nike/Fragment/Unisex.JPG`;
+const FragmentUnisex1 = `${baseUrl}/Nike/Fragment/Unisex 1.jpg`;
+const FragmentUnisex2 = `${baseUrl}/Nike/Fragment/Unisex.jpg`;
 
-const GiannisMen = `${baseUrl}/Nike/Giannis/Men size.JPG`;
+const GiannisMen = `${baseUrl}/Nike/Giannis/Men size.jpg`;
 
-const JoyrideUnisex3 = `${baseUrl}/Nike/Joyride/Unisex 3.JPG`;
-const JoyrideUnisex1 = `${baseUrl}/Nike/Joyride/Unisex 1.JPG`;
-const JoyrideUnisex2 = `${baseUrl}/Nike/Joyride/Unisex 2.JPG`;
-const JoyrideUnisex4 = `${baseUrl}/Nike/Joyride/Unisex.JPG`;
+const JoyrideUnisex3 = `${baseUrl}/Nike/Joyride/Unisex 3.jpg`;
+const JoyrideUnisex1 = `${baseUrl}/Nike/Joyride/Unisex 1.jpg`;
+const JoyrideUnisex2 = `${baseUrl}/Nike/Joyride/Unisex 2.jpg`;
+const JoyrideUnisex4 = `${baseUrl}/Nike/Joyride/Unisex.jpg`;
 
-const AlphabounceUnisex1 = `${baseUrl}/Adidas/Alphabounce/Unisex 1.JPG`;
-const AlphabounceMen1 = `${baseUrl}/Adidas/Alphabounce/Men size.JPG`;
-const AlphabounceUnisex2 = `${baseUrl}/Adidas/Alphabounce/Unisex 2.JPG`;
-const AlphabounceUnisex3 = `${baseUrl}/Adidas/Alphabounce/Unisex.JPG`;
+const AlphabounceUnisex1 = `${baseUrl}/Adidas/Alphabounce/Unisex 1.jpg`;
+const AlphabounceMen1 = `${baseUrl}/Adidas/Alphabounce/Men size.jpg`;
+const AlphabounceUnisex2 = `${baseUrl}/Adidas/Alphabounce/Unisex 2.jpg`;
+const AlphabounceUnisex3 = `${baseUrl}/Adidas/Alphabounce/Unisex.jpg`;
 
-const StansmithUnisex1 = `${baseUrl}/Adidas/Stansmith/Unisex 1.JPG`;
-const StansmithUnisex2 = `${baseUrl}/Adidas/Stansmith/Unisex.JPG`;
+const StansmithUnisex1 = `${baseUrl}/Adidas/Stansmith/Unisex 1.jpg`;
+const StansmithUnisex2 = `${baseUrl}/Adidas/Stansmith/Unisex.jpg`;
 
-const UltraboostUnisex1 = `${baseUrl}/Adidas/Ultraboost/Unisex 1.JPG`;
-const UltraboostUnisex2 = `${baseUrl}/Adidas/Ultraboost/Unisex.JPG`;
-const UltraboostWomen = `${baseUrl}/Adidas/Ultraboost/Women size.JPG`;
+const UltraboostUnisex1 = `${baseUrl}/Adidas/Ultraboost/Unisex 1.jpg`;
+const UltraboostUnisex2 = `${baseUrl}/Adidas/Ultraboost/Unisex.jpg`;
+const UltraboostWomen = `${baseUrl}/Adidas/Ultraboost/Women size.jpg`;
 
-const YeezyUnisex1 = `${baseUrl}/Adidas/Yeezy/Unisex 1.JPG`;
-const YeezyMen = `${baseUrl}/Adidas/Yeezy/Men size.JPG`;
-const YeezyUnisex2 = `${baseUrl}/Adidas/Yeezy/Unisex 2.JPG`;
-const YeezyUnisex4 = `${baseUrl}/Adidas/Yeezy/Unisex 4.JPG`;
+const YeezyUnisex1 = `${baseUrl}/Adidas/Yeezy/Unisex 1.jpg`;
+const YeezyMen = `${baseUrl}/Adidas/Yeezy/Men size.jpg`;
+const YeezyUnisex2 = `${baseUrl}/Adidas/Yeezy/Unisex 2.jpg`;
+const YeezyUnisex4 = `${baseUrl}/Adidas/Yeezy/Unisex 4.jpg`;
 
 module.exports = {
     AF1Unisex1,
