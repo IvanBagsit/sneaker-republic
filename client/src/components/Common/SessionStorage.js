bc004d8b6d2c50382d86db4a373f8df84683fb6b
ivanbagsit <ivan.bagsit@novare.com.hk>
2023-12-19 15:37:55 +0800

    added save and get username in SessionStorage

--- client/src/components/Common/SessionStorage.js
+++ client/src/components/Common/SessionStorage.js
@@ -6,3 +6,12 @@ export const handleGetIsStarted = () => {
     const savedData = sessionStorage.getItem("isStarted");
     return savedData;
 };
+
+export const saveUsername = (value) => {
+    sessionStorage.setItem("srUsername", value);
+};
+
+export const getUsername = () => {
+    const savedUsername = sessionStorage.getItem("srUsername");
+    return savedUsername;
+};
